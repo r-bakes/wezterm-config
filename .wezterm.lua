bb1ed0f90364a19a38eb29207011751f38162a6f
Riley Bakes <48607876+r-bakes@users.noreply.github.com>
2025-07-03 21:38:52 -0400

    Initial Commit

---
+++ .wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- customize below --
+
+config.color_scheme = "nordfox"
+-- config.color_scheme = "nord-light"
+
+-- customize above --
+return config
